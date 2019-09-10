432a6798d5ca6a5b9873fc37112956f5370f5f5d
congweitao <congweitao@hotmail.com>
2019-09-10 16:56:06 +0800

    add packages

--- jazz_age/jazz_age/urls.py
+++ jazz_age/jazz_age/urls.py
@@ -15,7 +15,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.urls import include
+from django.views.generic import RedirectView
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-]
+    path('jazz_video/', include('jazz_video.urls')),
+    path('', RedirectView.as_view(url='/jazz_video/', permanent=True)),
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
---
+++ jazz_age/jazz_video/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class JazzVideoConfig(AppConfig):
+    name = 'jazz_video'
