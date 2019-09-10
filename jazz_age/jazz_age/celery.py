e5cd1edc20a52d18291919ac56a70253005f3575
congweitao <congweitao@hotmail.com>
2019-09-10 14:34:06 +0800

    bug

--- jazz_age/jazz_age/celery.py
+++ jazz_age/jazz_age/celery.py
@@ -3,8 +3,8 @@
 import os
 from celery import Celery
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE','jazz_time.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE','jazz_age.settings')
 
-celery_app = Celery('jazz_time')
+celery_app = Celery('jazz_age')
 celery_app.config_from_object('django.conf:settings', namespace='CELERY')
 celery_app.autodiscover_tasks()
