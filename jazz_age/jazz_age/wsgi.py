62c9e2bffb3e89310c43e2fdb9cc9018e9081554
congweitao <congweitao@hotmail.com>
2019-09-10 14:33:01 +0800

    change project name

--- jazz_age/jazz_age/wsgi.py
+++ jazz_age/jazz_age/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for jazz_time project.
+WSGI config for jazz_age project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'jazz_time.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'jazz_age.settings')
 
 application = get_wsgi_application()
--- jazz_age/manage.py
+++ jazz_age/manage.py
@@ -5,7 +5,7 @@ import sys
 
 
 def main():
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'jazz_time.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'jazz_age.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
